2f9ba229759831f5ab4d3c400b4df0cea93fd633
Keith Cortes <11corteschristopher@gmail.com>
2024-11-20 19:46:44 +0800

    com2

--- comp/glow.js
+++ comp/glow.js
@@ -10,37 +10,9 @@ import {
 } from "react-native";
 
 const MagicCard = () => {
-  const animation = useRef(new Animated.Value(1)).current;
-  const handlePressIn = () => {
-    Animated.spring(animation, { toValue: 0.8, useNativeDriver: true }).start();
-  };
-  const handlePressOut = () => {
-    Animated.spring(animation, {
-      toValue: 1,
-      friction: 3,
-      tension: 40,
-      useNativeDriver: true,
-    }).start();
-  };
-  const animatedStyle = { transform: [{ scale: animation }] };
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/xiaomi-pad.jpg")}
-        style={styles.img_background}
-      >
-        <Animated.View style={[styles.button, animatedStyle]}>
-          {" "}
-          <TouchableOpacity
-            activeOpacity={1}
-            onPressIn={handlePressIn}
-            onPressOut={handlePressOut}
-          >
-            {" "}
-            <Text style={styles.buttonText}>GET STARTED</Text>{" "}
-          </TouchableOpacity>{" "}
-        </Animated.View>
-      </ImageBackground>
+      <ImageBackground style={styles.imgBg}></ImageBackground>
     </View>
   );
 };
@@ -51,34 +23,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-
-  img_background: {
+  imgBg: {
     height: "100%",
     width: "100%",
-    display: "flex",
     resizeMode: "cover",
+    display: "flex",
     justifyContent: "center",
-  },
-
-  button: {
-    backgroundColor: "#6200ee",
-    paddingVertical: 20,
-    paddingHorizontal: 40,
-    borderRadius: 40,
-    elevation: 3,
-    width: 250,
-    position: "relative",
-    left: 55,
-    top: 200,
-    boxShadow: "0px 0px 12px white",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 23,
-    fontWeight: "bold",
-    position: "relative",
-    left: 8,
-    filter: "drop-shadow(0px 0px 12px white)",
   },
 });
 
